7360c6f3e8a148863bd3f2f1eeacc48b05e852a0
Smith Barros <80532010+Smithbarros@users.noreply.github.com>
2022-09-21 14:43:16 -0300

    soma

--- 11_quest.sh
+++ 11_quest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Exercício 11 - Desenvolva um script que realize as operações aritméticas básicas (soma, subtração, divisão e multiplicação), recebendo a operação e os operadores como parâmetros.
-
-
-
-
+if [ $2 == "+" ]
+    then
+    echo "$1 + $3 = $(($1+$3))"
+fi
