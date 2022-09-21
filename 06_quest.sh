5e4438bc60b5a7779a80df55c458a761931a8c6c
Smith Barros <80532010+Smithbarros@users.noreply.github.com>
2022-09-21 13:18:08 -0300

    menu da 6 questão

--- 06_quest.sh
+++ 06_quest.sh
@@ -1,21 +1,9 @@
-res=0
-
-while [ $res -ne 4 ]; do
-  echo "(1) Status da utilização das partições do sistema."
-  echo "(2) Relação de usuários logados."
-  echo "(3) Data/Hora"
-  echo "(4) Sair" && echo
-  echo "Informe sua opção:"
-  read res
-	
-  ## Preencha com cada uma das opções
-  case $res in
-   1) echo "" 
-   ;;
-   2) echo ""
-   ;;
-   3) echo ""
-   ;;
-   *) echo ""
-   esac
-done
+#!/bin/bash
+MENU="
+                            MENU         
+1- Exibir status da utilização das partições do sistema
+2- Exibir relação de usuário logados
+3- Data e Hora
+4- Sair
+"
+echo "$MENU"
