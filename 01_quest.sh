6af3b549cd34c133828040de152e76d1a84f7d8c
Smith Barros <80532010+Smithbarros@users.noreply.github.com>
2022-09-26 21:07:21 -0300

    finalizado

--- 01_quest.sh
+++ 01_quest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#test da bransh
+
+# 1º questão
+#Elaborar um script que receba dois números como parâmetros e como resultado da sua execução exiba as seguintes informações: 
+# O maior dos números recebidos; 
+# O somatório dos números recebidos.
+
+read $1, $2;
+
 if [ $1 -lt $2 ]
 then
 	echo "$2 eh o maior numero"
