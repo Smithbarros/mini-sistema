62e9cbe6e061d08705812ab2b7f29ca2e289d664
Maycon369 <113936023+Maycon369@users.noreply.github.com>
2022-09-28 14:48:28 -0300

    Update 03_quest.sh
    concluido

--- 03_quest.sh
+++ 03_quest.sh
@@ -12,3 +12,21 @@ for temp in $(ls $caminho); do # DICA: a cada iteracao a variavel será um arqui
 		echo "$temp"
 	fi
 done
+
+
+
+
+
+
+#!/bin/bash
+#recebe o caminho de um diretóriio e listar o conteúdo um a um. se for diretorio, escrever "(dir)" ao lado do diretorio.
+echo "Digite o diretorio"
+read dir
+if [ -d "$dir" ]
+then
+for coisa in $(ls $dir) do 
+echo "$coisa $([ -d$dir/ $coisa ] && echo "(dir)")"
+done
+else
+echo "Não existe" && exit
+fi
